22119e831457bd9a8e886a7b816d2685a94bc8f8
Lázaro <joselazarosiqueira@gmail.com>
2018-01-03 12:54:26 -0200

    Implementing flight component

--- src/app/components/flight/flight.component.ts
+++ src/app/components/flight/flight.component.ts
@@ -1,3 +1,6 @@
+import { ExceptionService } from './../../services/exception.service';
+import { NewDataSource } from './../grid/datasources';
+import { DatePipe } from '@angular/common';
 import { environment } from './../../../environments/environment';
 import { Flight } from './../../models/flight';
 import { Http } from '@angular/http';
@@ -9,16 +12,37 @@ import { SpinnerService } from '../../services/spinner.service';
 import { TranslateService } from '@ngx-translate/core';
 import { FlightService } from './../../services/flight.service';
 
+export class FlightVO {
+  flightNumber: string;
+  from: string;
+  to: string;
+  status: string;
+  departureDateTime: any;
+  arrivalDateTime: any;
+  id: any;
+}
+
 @Component({
   selector: 'app-flight',
   templateUrl: './flight.component.html',
   styleUrls: ['./flight.component.scss'],
   providers:
   [
-    SpinnerService, FlightService
+    SpinnerService, FlightService, DatePipe
   ]
 })
 export class FlightComponent implements OnInit {
+
+  defColumnsGrid = [
+    { value: 'flightNumber', display: 'Número' },
+    { value: 'from', display: 'Origem' },
+    { value: 'to', display: 'Destino' },
+    { value: 'departureDateTime', display: 'Data/Hora de Partida' },
+    { value: 'arrivalDateTime', display: 'Data/Hora de Chegada' },
+    { value: 'status', display: 'Status' }
+  ];
+
+  public dataSource: any;
 
   constructor(
     private vcr: ViewContainerRef,
@@ -28,15 +52,32 @@ export class FlightComponent implements OnInit {
     private toastr: ToastsManager,
     private spinnerService: SpinnerService,
     private flightService: FlightService,
+    private datePipe: DatePipe,
+    private exceptionService: ExceptionService
   ) {
     this.spinnerService.setRootViewContainerRef(this.vcr);
   }
 
   ngOnInit() {
+    this.spinnerService.activate();
     this.flightService.getFlights()
       .subscribe(
         (response: Flight[]) => {
-          // to implement
+          const data = [];
+          for (const item of response){
+            const flightVO = new FlightVO();
+            flightVO.id = item.id;
+            flightVO.arrivalDateTime = item.arrivalDateTime;
+            flightVO.departureDateTime = item.departureDateTime;
+            flightVO.flightNumber = item.flightNumber;
+            flightVO.from = item.from.name;
+            flightVO.to = item.to.name;
+            flightVO.status = this.translateService.instant('FLIGHT.STATUS.' + item.status);
+            data.push(flightVO);
+          }
+
+          this.dataSource = new NewDataSource(data, null);
+          this.spinnerService.deactivate();
         },
         (error: any) => {
         }
